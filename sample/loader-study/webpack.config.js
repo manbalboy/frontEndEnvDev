8fae75d3aa80f583baafd28979f123b7bb494e91
manbalboy <manbalboy@hanmail.net>
2021-03-07 15:18:24 +0900

    MiniCssExtractPlugin,  CleanWebpackPlugin, HtmlWebpackPlugin 학습

--- sample/loader-study/webpack.config.js
+++ sample/loader-study/webpack.config.js
@@ -1,7 +1,9 @@
 const path = require('path');
 const webpack = require('webpack');
-
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MyWebpackPlugin = require('./my-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 module.exports = {
     mode : 'development',
     entry :{
@@ -16,7 +18,7 @@ module.exports = {
             {
                 test :  /\.css$/,
                 use : [
-                    'style-loader',
+                    process.env.NOD_ENV === 'production' ? MiniCssExtractPlugin.loader : 'style-loader',
                     'css-loader'
                 ]
             },
@@ -44,9 +46,28 @@ module.exports = {
         ]
     },
     plugins : [
-        new webpack.BannerPlugin({
+        new webpack.BannerPlugin({ //파일에 주석
             banner : `Build Date : ${new Date().toLocaleString() }`
-        })
+        }),
+        new webpack.DefinePlugin({ //환경변수 선언
+            TWO : JSON.stringify('1+1'),
+        }),
+        new HtmlWebpackPlugin({ //html 관리 
+            template : './src/index.html',
+            templateParameters : {
+                // env : process.env.NOD_ENV === 'development' ? '(개발용)' : ''
+                envTest :  '(개발용)' 
+            },
+            minify :{ 
+                collapseWhitespace : true, // 빈칸제거
+                removeComments : true // 주석제거
+            }
+        }),
+        new CleanWebpackPlugin(), //output 삭제 
+        ...(process.env.NOD_ENV === 'production' ?
+            [new MiniCssExtractPlugin({filename : '[name].css'})]
+            : [])
+
         // new MyWebpackPlugin(),
     ]
 }
