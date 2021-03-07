741eafb25a6b315248344a46371cc82f014d62b9
manbalboy <manbalboy@hanmail.net>
2021-03-07 10:26:25 +0900

    로더 학습 css,style,file

--- sample/history/webpack.config.js
+++ sample/history/webpack.config.js
@@ -2,10 +2,11 @@ const path = require('path');
 module.exports = {
     mode : 'development',
     entry :{
-        main :'./history/app.js'
+        // main :'./history/app.js' 엔트리와 아웃풋 실험 , 웹팩로더 실험
+        main :'./app.js'
     },
     output : {
-        path : path.resolve('./history/dist'),
+        path : path.resolve('./dist'),
         filename: '[name].js'
     },
     module: {
---
+++ sample/loader-study/webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+module.exports = {
+    mode : 'development',
+    entry :{
+        main :'./src/app.js'
+    },
+    output : {
+        path : path.resolve('./dist'),
+        filename: '[name].js'
+    },
+    module: {
+        rules: [
+            {
+                test :  /\.css$/,
+                use : [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test :  /\.png$/,
+                loader : 'file-loader',
+                options : {
+                    publicPath : './dist/',
+                    name  : '[name].[ext]?[hash]'
+                } 
+                // use : [
+                //     'file-loader'
+                // ]
+            }
+        ]
+    }
+}
