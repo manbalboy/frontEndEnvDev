175adb4c6288184eb64e68d95590a8684cd1734e
manbalboy <manbalboy@hanmail.net>
2021-03-07 11:49:01 +0900

    loader 실습

--- 02.webpack-loader/webpack.config.js
+++ 02.webpack-loader/webpack.config.js
@@ -1,12 +1,31 @@
 const path = require('path');
 
 module.exports = {
-  mode: 'development',
-  entry: {
-    main: './src/app.js'
-  },
-  output: {
-    filename: '[name].js',
-    path: path.resolve('./dist'),
-  }
+    mode: 'development',
+    entry: {
+        main: './src/app.js'
+    },
+    output: {
+        filename: '[name].js',
+        path: path.resolve('./dist'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },{
+                test: /\.(png|jpg)$/i,
+                loader : 'url-loader',
+                options : {
+                    publicPath : './dist/',
+                    name  : '[name].[ext]?[hash]',
+                    // limit : 20000, //20kb
+                }
+            }
+        ]
+    }
 }
